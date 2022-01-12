2d255703ba6c21c0b61dd2cf41dcc803d3f5dba6
Adithiya <97518508+Adicurry3000@users.noreply.github.com>
2022-01-12 11:47:15 +0530

    Update settings.py

--- youtube_search_app/settings.py
+++ youtube_search_app/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^i%!c)v*bpw92t%u96!#js8$h(kax=+zqcrxd*um8ho2rs$gh('
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
